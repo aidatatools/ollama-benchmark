e3c03080cdda64679cb6415f51409536cb2ddd23
Jason Chuang <chuangtcee@gmail.com>
2024-06-11 02:41:04 +0800

    support for smaller size of memory for SBCs

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='llm_benchmark',
-    version='0.3.18',
+    version='0.3.20',
     author='Jason Chuang',
     author_email='chuangtcee@gmail.com',
     description='LLM Benchmark',
