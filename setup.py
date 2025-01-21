ac3fd89d359067be0cef75b1d7f8fbed85e902a7
Jason Chuang <chuangtcee@gmail.com>
2025-01-22 07:45:27 +0800

    setup.py up to v0.3.30

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='llm_benchmark',
-    version='0.3.29',
+    version='0.3.30',
     author='Jason Chuang',
     author_email='chuangtcee@gmail.com',
     description='LLM Benchmark',
