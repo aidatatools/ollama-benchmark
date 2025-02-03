a08bbb6021ec5edbeb92f446998f35c20e90802b
Jason Chuang <chuangtcee@gmail.com>
2025-02-04 00:21:37 +0800

    bump up to v0.4.1 for deepseek-r1 variations

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='llm_benchmark',
-    version='0.4.0',
+    version='0.4.1',
     author='Jason Chuang',
     author_email='chuangtcee@gmail.com',
     description='LLM Benchmark',
